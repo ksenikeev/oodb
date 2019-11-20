0cd910d4cb87008df9c33f97fc589c1f5d272337
Kamil Enikeev <ksenikeev@mail.ru>
2019-11-20 10:22:40 +0300

    lab 6 scan package

--- src/main/java/ru/icmit/oodb/lab6/Lab6Main.java
+++ src/main/java/ru/icmit/oodb/lab6/Lab6Main.java
@@ -1,10 +1,40 @@
 package ru.icmit.oodb.lab6;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
 public class Lab6Main {
 
+    public static String PATH_FOR_SCAN = "ru.icmit.oodb.lab6.domain";
 
     public static void main(String[] args) {
-        PathScan.find("ru.icmit.oodb.lab6.domain")
-                .forEach(c->System.out.println(c.getCanonicalName()));
+
+        /* Просканируем пакет PATH_FOR_SCAN для поиска классов (включая вложенные пакеты)  */
+        System.out.println("STEP 1: scan package " + PATH_FOR_SCAN+":");
+
+        List<Class<?>> classList = PathScan.find(PATH_FOR_SCAN);
+        if (classList != null)
+            classList.forEach(c->System.out.println("\t" + c.getCanonicalName()));
+
+        System.out.println("STEP 2: scan class fields:");
+        for (Class<?> cl : classList) {
+            /* Сканируем поля классов */
+            System.out.println("\tFields of class " + cl.getName());
+            Field[] fields = cl.getDeclaredFields();
+            for (Field field : fields) {
+                System.out.println("\t\t" + field.getName());
+            }
+        }
+
+        System.out.println("STEP 3: scan class methods:");
+        for (Class<?> cl : classList) {
+            /* Сканируем методы классов */
+            System.out.println("\tFields of class " + cl.getName());
+            Method[] methods = cl.getMethods();
+            for (Method method : methods) {
+                System.out.println("\t\t" + method.getName());
+            }
+        }
     }
 }
